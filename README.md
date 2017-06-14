bcfbc682be217325e16036603cf23dfc1b4c587f
Thomas Keck <thomas@goodmorningmajor.com>
2017-06-14 18:22:48 +0200

    add onComplete and onError callbacks

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # PrestaShop Cache Clear
+[![npm version](https://badge.fury.io/js/prestashop-cache-clear.svg)](https://badge.fury.io/js/prestashop-cache-clear)
 
 > Clear your PrestaShop 1.6 project cache in command line.
 
@@ -15,6 +16,26 @@ var psClear = require('prestashop-cache-clear');
 
 // Clear PrestaShop cache
 psClear.clear();
+```
+
+## Options
+
+### onComplete
+```js
+psClear.clear({
+  onComplete: function () {
+    // Called after cache cleared.
+  }
+});
+```
+
+### onError
+```js
+psClear.clear({
+  onError: function () {
+    // Called after an error.
+  }
+});
 ```
 
 ## Note
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var chalk = require('chalk');
 var spawn = require('child_process').spawn;
 
-exports.clear = function() {
+exports.clear = function(options) {
+  var options = typeof options === 'object' ? options : {};
+  var onComplete = typeof options.onComplete === 'function' ? options.onComplete : function(){};
+  var onError = typeof options.onError === 'function' ? options.onError : function(){};
   var cmd = spawn('php', [
     '-r',
     'include("' + __dirname + '/cacheclear.php");'
@@ -15,8 +18,10 @@ exports.clear = function() {
   cmd.on('exit', function (code) {
     if (code === 0) {
       console.log(chalk.green('Cache cleared'));
+      onComplete();
     } else {
       console.log(chalk.red('Error during clear cache'));
+      onError();
     }
   });
 }
